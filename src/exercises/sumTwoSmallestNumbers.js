71a99fc3b23459f89a8611ff9d63cb4d79a793ee
asdf <alexrayo.net@gmail.com>
2025-07-10 16:37:51 -0600

    first commit

---
+++ src/exercises/sumTwoSmallestNumbers.js
@@ -0,0 +1,6 @@
+export function sumTwoSmallestNumbers(numbers) {  
+  const min1 = Math.min(...numbers);
+  numbers.splice(numbers.indexOf(min1), 1);
+  const min2 = Math.min(...numbers);
+  return min1 + min2;
+}
---
+++ src/main.js
@@ -0,0 +1,34 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
+//
+import { sumTwoSmallestNumbers } from './exercises/sumTwoSmallestNumbers.js';
+import { likes } from './exercises/likes.js';
+import { getSumBetween } from './exercises/getSumBetween.js';
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
+
+console.log('sumTwoSmallestNumbers:' , sumTwoSmallestNumbers([1, 1, 2, 3]))
+console.log('likes:', likes(['John', 'Jane', 'Jack']))
+console.log('getSumBetween:', getSumBetween(-1, 2))
+
+
